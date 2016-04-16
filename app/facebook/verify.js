a202c0dc61320b423505f5cde24bdfe0354dd695
Brad Harris <bmharris@gmail.com>
2016-04-16 10:44:37 -0600

    fixing verify route

--- app/facebook/verify.js
+++ app/facebook/verify.js
@@ -3,11 +3,8 @@
 module.exports = (app) => {
   var verifyToken = app.config.facebook.verifyToken
   return (req, res) => {
-    var hub = req.query.hub || {}
-    app.log.facebook('verify: ', req.query)
-
-    if (hub.mode === 'subscribe' && hub.verify_token === verifyToken) {
-      return res.send(hub.challenge)
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === verifyToken) {
+      return res.send(req.query['hub.challenge'])
     }
 
     res.sendStatus(400)
