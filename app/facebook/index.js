225d0fe7c54965e5e300a1b65bb782771ffe771d
Brad Harris <bmharris@gmail.com>
2016-04-16 10:53:41 -0600

    adding body parser

--- app/facebook/index.js
+++ app/facebook/index.js
@@ -1,10 +1,11 @@
 const Router = require('express').Router
+const bodyParser = require('body-parser')
 
 module.exports = (app) => {
   var router = Router()
 
   router.get('/webhook', require('./verify')(app))
-  router.post('/webhook', require('./message')(app))
+  router.post('/webhook', bodyParser.json(), require('./message')(app))
 
   return router
 }
